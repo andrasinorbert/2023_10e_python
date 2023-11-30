75f41f8fbf4d9cd56a082c800c62ddb02c825031
andrasinorbert <andrasinorbert.puskasttt@gmail.com>
2023-11-30 11:01:03 +0100

    update

---
+++ 20231114.py
@@ -0,0 +1,97 @@
+print("Hello!")
+
+for i in range(10):
+    print(i, end=" ")
+print()
+print("szevasz", "világ", sep="-")
+
+print("4\\5")
+print("\n ujsor")
+print("\t tabulátor")
+
+print("ha"*5)
+
+x=123
+y="szia"
+
+print(10_321_654_789)
+
+x=int("15", 8)
+print(x)
+
+x=0o15
+print(x)
+x=0xABBA
+print(x)
+x=0b10101000
+print(x)
+
+print(3E8)
+x=3* 10**200
+print(x)
+print(0.0000000000000000000001)
+
+print('Anyukám azt mondta hogy: "Érj haza időben!"')
+print("Anyukám azt mondta hogy: \"Érj haza időben!\"")
+
+x= True+True
+print(x)
+
+x=2**(3**4)
+print(x)
+x= (9 % 6) % 2
+#y= 9 % (6 % 2)
+z=9 % 6 % 2
+print(x)
+#print(y)
+print(z)
+
+x=8//3
+y=int(8/3)
+print(x)
+print(y)
+
+_ =7
+
+def fv():
+    print(5)
+
+fv=6
+
+alma=5
+Alma=6
+
+print(alma)
+print(Alma)
+
+almá=5
+alma=6
+
+print(almá)
+print(alma)
+
+print(print(5))
+x=print(15)
+print(x==None)
+print(type(x))
+
+def valami():
+    print(76)
+
+x=valami()
+print(x==None)
+
+"""
+ez
+egy
+komment
+"""
+
+def valami():
+    """
+    komment
+    """
+    print(66)
+
+valami()
+
